41302a363849df209815755a5d765769e7a6b5d2
Projets du Groupe PIGIER CI <45494036+projetpigier@users.noreply.github.com>
2026-04-25 08:58:32 +0000

    Add files via upload

---
+++ ExempleHeritage.java
@@ -0,0 +1,27 @@
+package ci.pigier;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class ExempleHeritage extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Texte hérité");
+
+        VBox root = new VBox(label);
+        root.setStyle("-fx-font-size: 48px;");
+
+        Scene scene = new Scene(root, 300, 200);
+        stage.setScene(scene);
+        stage.setTitle("Exemple d'Héritage");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ HelloFXApp02.java
@@ -0,0 +1,18 @@
+package ci.pigier;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class HelloFXApp extends Application {
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		// Set a title for the stage
+		 stage.setTitle("Hello JavaFX Application");
+		 // Show the stage
+		 stage.show();
+	}
+}
